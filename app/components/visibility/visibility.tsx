bcc5d00a79a60ec17531931b944cc528ec6f9950
Martin Rouault <martin.rlt24@gmail.com>
2024-07-02 09:44:02 +0200

    refactor: Update Visibility component to use currentWeather object instead of visibility

--- app/components/visibility/visibility.tsx
+++ app/components/visibility/visibility.tsx
@@ -7,23 +7,24 @@ import { EyeIcon } from "lucide-react";
 export default function Visibility() {
   const { currentWeather } = useGlobalContext();
 
-  const { visibility } = currentWeather;
+  const { current } = currentWeather;
 
-  if (!currentWeather || !visibility) {
+  if (!current) {
     return <Skeleton className="h-[12rem] w-full" />;
   }
   return (
     <div className="pt-6 pb-5 px-4 h-[12rem] border rounded-lg flex flex-col gap-8 shadow-sm dark:shadow-none">
       <div className="top">
         <h2 className="flex items-center gap-2 font-medium text-muted-foreground">
-          <EyeIcon size={20} />Visibility
+          <EyeIcon size={20} />
+          Visibility
         </h2>
-        <p className="pt-4 text-2xl">{visibility / 1000} km</p>
+        <p className="pt-4 text-2xl">{current.visibility / 1000} km</p>
       </div>
       <p className="text-sm">
-        {visibility / 1000 >= 10
+        {current.visibility / 1000 >= 10
           ? "Perfect visibility."
-          : visibility / 1000 >= 5
+          : current.visibility / 1000 >= 5
           ? "Good visibility."
           : "Poor visibility, be carful wile driving!"}
       </p>
